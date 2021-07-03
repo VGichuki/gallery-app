eda1a4396254608d42ba9a99dbe1d7729d4bc7fa
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-03 20:53:04 +0300

    create a search_results view function in the art/views.py folder

--- art/views.py
+++ art/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category,Location,Image
 
 # Create your views here.
 def welcome(request):
     #view function that renders the home page
     return render(request, 'welcome.html')
+
+def search_results(request):
+  if 'art' in request.GET and request.GET["art"]:
+    search_term=request.GET.get('art')
+    searched_category=Category.search_category(search_term)    
+    results=Image.search_image(searched_category)
+    if results:
+      message=f"{search_term}"  
+      title='Results'
+
+      return render(request,'search.html',{"message":message,"images":results,"title":title})
+
+    else:
+      message="You haven't search for a category"  
+      title='Results'
+      return render(request, 'search.html',{"message":message,"title":title})
 
