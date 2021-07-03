af7cd2dd6f824445b09020aea77f2bbb85a2f3bb
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-03 18:17:20 +0300

    run a test for the save function in class category

--- art/models.py
+++ art/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import datetime as dt
+
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Category(models.Model):
+    '''
+    class that categorises the images
+    '''
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+class Location(models.Model):
+    '''
+    class that defines the location of an image
+    '''
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    '''
+    class that contains the image details in the database
+    '''
+    # image_url = models.ImageField(upload_to='images/', blank = True)
+    image_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+    posted_on = models.DateField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- art/tests.py
+++ art/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Category, Location,Image
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    '''
+    class that will test the functions under class Category
+    '''
+    def setUp(self):
+        self.cg1 = Category(name = 'oil painting')
+        self.cg2 = Category(name = 'watercolor painitng')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cg1,Category))
+        self.assertTrue(isinstance(self.cg2, Category))
+    def test_save_method(self):
+        self.cg1.save_category()
+        self.cg2.save_category()
+        categories = Category.objects.all()
+        self.assertEquals(len(categories),2)
