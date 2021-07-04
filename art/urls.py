1c14ef54dbbc82c1358e5ff6aea0bb605bafb5cc
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-04 18:14:14 +0300

    create a route for viewing images once uploaded

--- art/urls.py
+++ art/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    path('images/<str:location_name>',views.images_by_location, name = "ImagesByLocation"),
+    path('category/<str:category_name>',views.images_by_category, name="ImagesByCategory"),
+
 ]
 
 if settings.DEBUG:
